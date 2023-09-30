94fa9fcd911224dccd382cd11a2d2f04998e3516
Alisa <112699771+AKulonpa@users.noreply.github.com>
2023-09-30 19:18:32 +0300

    Initial commit

---
+++ Data.js
@@ -0,0 +1,27 @@
+export const DATA = [
+    {
+        id: '1',
+        firstname: 'Erkki',
+        lastname: 'Juntunen'
+    },
+    {
+        id: '2',
+        firstname: 'Pirjo',
+        lastname:'Ojala'
+    },
+    {
+        id: '3',
+        firstname: 'Laura',
+        lastname: 'Karhunen'
+    },
+    {
+        id: '4',
+        firstname: 'Vertti',
+        lastname: 'Virtanen'
+    },
+    {
+        id: '5',
+        firstname: 'Eino',
+        lastname: 'Korhonen'
+    }
+]
